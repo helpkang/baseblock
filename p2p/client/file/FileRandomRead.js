49e29d8e38e13db4dbbe4d31d178d82e84e51ec2
helpkang <helpkang@gmail.com>
2018-02-25 21:57:06 +0900

    temp directory 생성

--- p2p/client/file/FileRandomRead.js
+++ p2p/client/file/FileRandomRead.js
@@ -10,28 +10,33 @@ class FileRandomRead {
     constructor() {
         this.fileName = './sample/sample.txt'
         this.hashFileName = './dist/sample'
+        this.createDir('./dist')
     }
-
+    createDir(dir) {
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir);
+        }
+    }
     async read(length = 10) {
         const fd = await fs.openAsync(this.fileName, 'r')
 
-        
+
         const buffer = new Buffer(length);
         const start = 0
 
         await fs.readAsync(fd, buffer, 0, length, start);
         return buffer
-        
+
     }
 
-   async makeHash(encoding=''){
+    async makeHash(encoding = '') {
         const stream = fs.createReadStream(this.fileName, {
             highWaterMark: 1024
         })
 
-        const wfd = await fs.openAsync(this.hashFileName+'.hash'+encoding, 'w')
+        const wfd = await fs.openAsync(this.hashFileName + '.hash' + encoding, 'w')
 
-        
+
         stream.on('data', (chunk) => {
             console.log(chunk.length)
             const hashfunc = crypto.createHash('rmd160')
@@ -45,12 +50,12 @@ class FileRandomRead {
         })
 
     }
-   async makeHashGzip(){
+    async makeHashGzip() {
         const stream = fs.createReadStream(this.fileName, {
             highWaterMark: 1024
         })
 
-        const out = fs.createWriteStream(this.hashFileName+'.hash.gzip');
+        const out = fs.createWriteStream(this.hashFileName + '.hash.gzip');
         const zipStream = zlib.createGzip();
         zipStream.pipe(out)
         stream.on('data', (chunk) => {
@@ -58,7 +63,7 @@ class FileRandomRead {
             const hashfunc = crypto.createHash('rmd160')
             const hash = hashfunc.update(chunk).digest()
             console.log('hash len', hash.length)
-            zipStream.write(hash, ()=>{ })
+            zipStream.write(hash, () => {})
 
         }).on('end', () => {
             zipStream.flush()
@@ -73,7 +78,7 @@ async function read() {
     const frr = new FileRandomRead()
     let l
     l = await frr.read(100)
-    if(l)console.log(l.toString())
+    if (l) console.log(l.toString())
 }
 
 // read()
