4a4e4da5056e8a8095bf3e221d1b1bf8896b3d15
성일 강 <1700429@partner.skcc.com>
2018-03-08 13:48:51 +0900

    오타 수정

--- p2p/client/handler/ClientListHandler.js
+++ p2p/client/handler/ClientListHandler.js
@@ -40,7 +40,7 @@ module.exports = class ClientListHandler extends CommandHandler {
 
       
     }
-    //TODO: tracekr에서 온걸 확인 하기 위해서 인증서 기반으로 처리 하다록 변경
+    //TODO: tracker 에서 온걸 확인 하기 위해서 인증서 기반 으로 처리 하다록 변경
     isServerMessage(remote) {
         return this.config.address === remote.address && this.config.port === remote.port
     }
